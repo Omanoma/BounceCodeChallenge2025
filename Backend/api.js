3541f1fc8e2f89c50736071e05f83c0c2b33cb82
Omanoma <omanoma14@gmail.com>
2025-02-20 19:30:09 +0000

    SetUp API

--- Backend/api.js
+++ Backend/api.js
@@ -1,1 +1,45 @@
-//test
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const today = new Date();
+const { APOD, EPIC,EPICIMAGE } = require("./nasaData");
+let app = express();
+
+app.use(cors());
+//app.use(express.static());
+const PORT = process.env.PORT || 4000;
+console.log(PORT)
+app.get("/APOD/:date", async function (req, res) {
+    let date = new Date(req.params.date)
+    let dateString1 = ((date.getMonth()+1)>=10)?`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`:`${date.getFullYear()}-0${date.getMonth()+1}-${date.getDate()}`
+    if(date>today){
+        res.send(`Must be 1995/03/16 and ${today.getFullYear()}/${today.getMonth()+1}/${today.getDate()}`)
+        return;   
+    }
+    let a = await APOD(dateString1);
+    res.send(a)
+})
+
+app.get("/EPIC/:date", async function(req, res){
+    let date = new Date(req.params.date)
+    if(date>today){
+        res.send(`Must be 1995/03/16 and ${today.getFullYear()}/${today.getMonth()+1}/${today.getDate()}`)
+        return;   
+    }
+    let dateString1 = ((date.getMonth()+1)>=10)?`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`:`${date.getFullYear()}-0${date.getMonth()+1}-${date.getDate()}`
+    let dateString = ((date.getMonth()+1)>=10)?`${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}`:`${date.getFullYear()}/0${date.getMonth()+1}/${date.getDate()}`
+    let image = []
+    let datas = await EPIC(dateString1);
+    for(let x in datas){
+        image[x] = await EPICIMAGE(dateString,datas[x].image);
+    }
+    
+    let result = {
+        inform: datas,
+        images: image
+    }
+    res.send(result)
+})
+
+app.listen(PORT);
--- Backend/nasaData.js
+++ Backend/nasaData.js
@@ -6,26 +6,35 @@ const apiKey = new Promise(function (resolve) {
     resolve(JSON.parse(data).Api);
   });
 });
-async function APOD(date) {
+ async function APOD(date) {
   let urladdress = `https://api.nasa.gov/planetary/apod?date=${date}&&api_key=${await apiKey}`;
   let data = await axios.get(urladdress).then(function (response) {
     return response.data;
+  }).catch(function (error) {
+    return error
   });
   return data;
 }
-async function EPIC(date) {
+ async function EPIC(date) {
   let urladdress = `https://api.nasa.gov/EPIC/api/natural/date/${date}?api_key=${await apiKey}`;
   let data = await axios.get(urladdress).then(function (response) {
     return response.data;
+  }).catch(function (error) {
+    return error
   });
-  return data
+  return data;
 }
 async function EPICIMAGE(date,imageID){
-    let urladdress = `https://api.nasa.gov/EPIC/archive/natural/${date}/png/${imageID}.png?api_key=${await apiKey}`;
-    let data = await axios.get(urladdress).then(function (response) {
-        return response.data;
-      });
-    return data
+    let urladdress = `https://api.nasa.gov/EPIC/archive/natural/${date}/png/${imageID}.png?api_key=${await apiKey}
+`;
+    //let data = await axios.get(urladdress).then(function (response) {
+    //  return response.data;
+    //}).catch(function (error) {
+    //  return error
+    //});
+    return urladdress;
 }
+
+module.exports = { APOD,EPIC,EPICIMAGE };
 
 
