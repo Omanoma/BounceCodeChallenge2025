cefb8bc48544280221a44c05577538f9293d8ab9
Omanoma <omanoma14@gmail.com>
2025-02-27 13:46:27 +0000

    add Epic Page

--- Frontend/frontend/src/pages/EPIC.js
+++ Frontend/frontend/src/pages/EPIC.js
@@ -5,8 +5,11 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
-import React from "react";
+import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
+import dateFormat from "dateformat";
+import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
 export function EPIC() {
   return (
     <div>
@@ -16,9 +19,14 @@ export function EPIC() {
 }
 export function Searchbar() {
   const [inputDate, setInputDate] = React.useState(new Date());
-  const [data, setData] = React.useState({});
-  const handleSubmit = (event) => {
+  const[dateSet,setDateSet] = React.useState(new Date());
+  const [data, setData] = React.useState(null);
+  const [pressButton, setButton] = useState(false);
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    setDateSet(inputDate);
+    await EPICAPI(inputDate, setData);
+    setButton(true);
   };
   return (
     <>
@@ -34,7 +42,6 @@ export function Searchbar() {
                 <Form.Control
                   type="date"
                   size="lg"
-                  name="D"
                   value={inputDate}
                   onChange={(event) => {
                     setInputDate(event.target.value);
@@ -51,27 +58,74 @@ export function Searchbar() {
           </Row>
         </Container>
       </Form>
+      <Container>
+    <br/>
+      {(pressButton)?<SlideShow data={data} date={dateSet}/>:<></>}
+      </Container>
+      
     </>
   );
 }
+export function EPICAPI(input, setData) {
+  let date = new Date() < new Date(input) ? new Date() : new Date(input);
+  let dateString = dateFormat(date, "yyyy-mm-dd");
+  let url = `https://bouncecodechallenge2025.onrender.com/EPIC/${dateString}`;
+  let result = axios.get(url).then(function (response) {
+    setData(response.data);
+    console.log(response.data)
+    return response.data;
+  });
+  return result;
+}
 
-export function slideShow(image, data) {
-  return (
-    <Carousel>
-      {image.map((key, i) => {
-        return (
+export function SlideShow(props) {
+    
+  return (<>
+  <h1>Earth Polychromatic Imaging Camera of {props.date}</h1>
+    <Carousel {...props} slide={false}>
+      {(props.data != null)? (
+        props.data.images.map((key, i) => {
+          return (
+            <Carousel.Item>
+              <Image className="d-block w-100" src={`${key}`} fluid/>
+              <Carousel.Caption>
+                <h3>{props.data.inform[i].date}  </h3>
+                <p>
+                    <li>
+                    <b>latitude</b>:{props.data.inform[i].centroid_coordinates.lat}
+                    </li>
+                    <li>
+                    <b>longution</b>{props.data.inform[i].centroid_coordinates.lon}
+                    </li>
+                </p>
+                <p>
+                    {props.data.inform[i].caption}
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })
+      ) : (
+        
           <Carousel.Item>
-            <img
-            className="d-block w-100"
-            src={key}
+            <Container>
+            <Image
+                fluid
+              src="https://fossbytes.com/wp-content/uploads/2017/09/Windows-Black-Screen-640x360.png"
+              className="d-block w-100"
+              alt="..."
             />
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <Spinner animation="border" role="status"variant="light">
+            <span className="visually-hidden">Loading...</span>
+            </Spinner>
+              <h1>Loading </h1>
             </Carousel.Caption>
+            </Container>
           </Carousel.Item>
-        );
-      })}
+        
+      )}
     </Carousel>
+    </>
   );
 }
